187e5668ca28e87bb40f21909866a923a848075e
sajjadmrx <sajjadzibaee102@gmail.com>
2022-08-13 17:16:44 +0430

    untrack dist

--- dist/best-string.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class BestString {
-    private string;
-    constructor(string: string);
-    noScriptTag(): this;
-    replaceGlobal(to: string, text: string): this;
-    noRtlCharacters(): this;
-    build(): string;
-}
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { BestString } from "./best-string";
-export default BestString;
